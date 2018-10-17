5cc0d072045c2b543c9631617be6148c847f5729
Thanuja Ambegoda <thanuja@enway.ai>
2018-10-17 15:16:45 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="impy",
+    version="0.0.1",
+    author="lozuwa",
+    author_email="github.com/lozuwa",
+    description="Image and label cropping tool",
+    url="https://github.com/lozuwa/impy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+)
